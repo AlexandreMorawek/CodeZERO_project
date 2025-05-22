62a30b2fa360490e1a9877207bb881e1cb1e2562
G-Ludovic <l.galicher@free.fr>
2025-05-22 15:00:26 +0200

    footer work in css done

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,18 @@ import "./Footer.css";
 function Footer() {
 	return (
 		<footer className="footer">
-			<h1>Ici le pied de page</h1>
+			<p>&copy; Protojam 2025</p>
+			<figure className="grid-figure">
+				<a href="https://github.com/" target="blank">
+					<img
+						src="src/assets/images/github-mark-white.png"
+						alt="logo GitHub"
+					/>
+				</a>
+				<a href="https://www.wildcodeschool.com/" target="blank">
+					<img src="src/assets/images/LogoWildCodeSchool.png" alt="logo Wild" />
+				</a>
+			</figure>
 		</footer>
 	);
 }
