91042162a3e269a9e0b20e4d3ada6ca114f82588
G-Ludovic <l.galicher@free.fr>
2025-05-22 11:25:43 +0200

    tailwindcss done

---
+++ src/Components/Carte.tsx
@@ -0,0 +1,24 @@
+function Carte() {
+	return (
+		<section className="">
+		<div className="w-[80%] mx-auto pt-5">
+			<h2 className="bg-secondary text-white py-4 px-3 text-center rounded-tr-lg rounded-tl-lg">
+				1. Quel langage est utilisé pour structurer une page web ?
+			</h2>
+			<section className="bg-white shadow-2xl flex-col rounded-br-lg rounded-bl-lg">
+			<div className=" text-secondary py-4 text-center md:grid md:grid-cols-2">
+				<p className="py-3 md:py-7 cursor-pointer">1. CSS</p>
+				<p className="py-3 md:py-7 cursor-pointer">2. HTML</p>
+				<p className="py-3 md:py-7 cursor-pointer">3. JavaScript</p>
+				<p className="py-3 md:py-7 cursor-pointer">4. PHP</p>
+			</div>
+			<div className="flex justify-center">
+				<button className="bg-secondary text-white px-7 py-2 rounded-full mb-5 cursor-pointer" type="button">Valider</button>
+				</div>
+			</section>
+		</div>
+		</section>
+	);
+}
+
+export default Carte;
