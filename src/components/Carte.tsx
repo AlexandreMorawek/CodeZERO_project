26209cfaf9dfa515a8ea1655581f3bfa8be594e2
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-05-22 12:20:06 +0200

    valid version

--- src/components/Carte.tsx
+++ src/components/Carte.tsx
@@ -17,10 +17,10 @@ function Carte ({question, answer1, answer2, answer3, answer4} : questionType)  
 			</h2>
 			<section className="bg-white shadow-2xl flex-col rounded-br-lg rounded-bl-lg">
 			<div className=" text-secondary py-4 text-center md:grid md:grid-cols-2">
-				<p className="py-3 md:py-7 cursor-pointer">{answer1}</p>
-				<p className="py-3 md:py-7 cursor-pointer">{answer2}</p>
-				<p className="py-3 md:py-7 cursor-pointer">{answer3}</p>
-				<p className="py-3 md:py-7 cursor-pointer">{answer4}</p>
+				<p className="py-3 md:my-7 cursor-pointer">{answer1}</p>
+				<p className="py-3 md:my-7 cursor-pointer">{answer2}</p>
+				<p className="py-3 md:my-7 cursor-pointer">{answer3}</p>
+				<p className="py-3 md:my-7 cursor-pointer">{answer4}</p>
 			</div>
 			<div className="flex justify-center">
 				<button className="bg-secondary text-white px-7 py-2 rounded-full mb-5 cursor-pointer" type="button">Valider</button>
