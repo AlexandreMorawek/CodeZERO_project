cec88f856d4f49016527ac47d1c9c43b88ad6c4f
G-Ludovic <l.galicher@free.fr>
2025-05-22 11:51:34 +0200

    header css done & router

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import HomePage from "./pages/HomePage/HomePage";
 
 const router = createBrowserRouter([
 	{
 		element: <App />,
 		path: "/",
-		children: [
-			{
-				element: <HomePage />,
-				path: "/",
-			},
-		],
 	},
 ]);
 
