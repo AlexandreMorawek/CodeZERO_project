e2d1a5fe3e1ccc64a600c1b511382343f656fdc0
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-05-23 10:22:55 +0200

    moving description component to questionList

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,9 @@
 import "./HomePage.css";
 import QuestionList from "../../Components/questionList";
-import Description from "../../Components/Description/description";
 
 function HomePage() {
 	return (
 		<>
-		<Description />
 		<QuestionList />
 		</>
 	);
