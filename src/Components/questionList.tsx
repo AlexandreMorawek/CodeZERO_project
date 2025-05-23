b350642308e82a654476857b07b7ede1d474fcd9
DURNERIN NATHAN <durnerin.nathan01@gmail.com>
2025-05-23 10:38:41 +0200

    fix css

--- src/Components/questionList.tsx
+++ src/Components/questionList.tsx
@@ -7,7 +7,7 @@ import Description from "./Description/description";
 function QuestionList() {
 	const [startQuiz, setStartQuiz] = useState(false);
 	const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [quizFinished, setQuizFinished] = useState(false)
+	const [quizFinished, setQuizFinished] = useState(false);
 
 	const handleNextQuestion = () => {
 		if (currentQuestion < data.length - 1) {
@@ -22,31 +22,33 @@ function QuestionList() {
 	return (
 		<>
 			{!startQuiz && (
-				<div className="button1 h-[39vh] flex flex-col items-center justify-center gap-y-7">
-					<button
-						type="button"
-						className="quiz_button bg-orange-700 text-white px-7 py-2 rounded-full cursor-pointer"
-					>
-						Selectionner la difficulté
-					</button>
-					<button
-						type="button"
-						className="quiz_button bg-orange-700 text-white px-7 py-2 rounded-full cursor-pointer"
-						onClick={() => setStartQuiz(true)}
-					>
-						Commencer le quiz !
-					</button>
-					<button
-						type="button"
-						className="quiz_button bg-orange-700 text-white px-7 py-2 rounded-full cursor-pointer"
-					>
-						Récapitulatif de mes scores
-					</button>
-				</div>
-                </>
+				<>
+					<Description />
+					<div className="button1 h-[39vh] flex flex-col items-center justify-center gap-y-7">
+						<button
+							type="button"
+							className="quiz_button bg-orange-700 text-white px-7 py-2 rounded-full cursor-pointer"
+						>
+							Selectionner la difficulté
+						</button>
+						<button
+							type="button"
+							className="quiz_button bg-orange-700 text-white px-7 py-2 rounded-full cursor-pointer"
+							onClick={() => setStartQuiz(true)}
+						>
+							Commencer le quiz !
+						</button>
+						<button
+							type="button"
+							className="quiz_button bg-orange-700 text-white px-7 py-2 rounded-full cursor-pointer mb-10"
+						>
+							Récapitulatif de mes scores
+						</button>
+					</div>
+				</>
 			)}
 			{startQuiz && (
-				<div className="p-[12rem]">
+				<div className="pt-[12rem] pb-[6rem]">
 					<Carte
 						key={data[currentQuestion].id}
 						question={data[currentQuestion].question}
@@ -61,11 +63,11 @@ function QuestionList() {
 					/>
 				</div>
 			)}
-            {quizFinished && (
-                <div className="text-white text-center p-[2rem]">
-                    Quiz terminé ! Merci d'avoir participé.
-                </div>
-            )}
+			{quizFinished && (
+				<div className="text-white text-center p-[2rem]">
+					Quiz terminé ! Merci d'avoir participé.
+				</div>
+			)}
 		</>
 	);
 }
